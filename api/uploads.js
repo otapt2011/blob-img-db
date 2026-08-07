bca9f9b6419b7cb4f7bd4a4cca9b4d9407ed57fd
JaferF <otapt2011@gmail.com>
2026-08-08 01:28:15 +0300

    Update uploads.js

--- api/uploads.js
+++ api/uploads.js
@@ -67,6 +67,9 @@ export default async function handler(req, res) {
       contentType: file.mimetype || 'application/octet-stream',
       token: BLOB_READ_WRITE_TOKEN,
       addRandomSuffix: false,
+  metadata: {
+    lastModified: String(lastModified),
+  },
     });
 
     return res.status(200).json(blob);
